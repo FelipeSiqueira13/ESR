01f50b42ebdbc422eb2fec885d3a921e5471093a
Sofia Oliveira <asro8589@gmail.com>
2025-11-25 14:03:00 +0000

    added get_streams in server_database

--- server_database.py
+++ server_database.py
@@ -8,7 +8,22 @@ class DataBase():
 
     def __init__(self, name):
 
-        with open('../config/server_config.json', 'r') as file:
+        with open('server_config.json', 'r') as file:
             server_info = json.load(file)
         
         self.server_streams = server_info.get(name, {}).get("streams", {})
+
+
+
+    def get_streams(self):
+        """Retorna lista de ids streams"""
+        return list(self.server_streams.keys())
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        db = DataBase("R1")   # alterar depois no trabalho final
+    else:
+        db = DataBase(sys.argv[1])
+
+    print(db.get_streams())
